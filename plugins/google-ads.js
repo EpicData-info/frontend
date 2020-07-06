0710b60bd636baf2e726292bc67a3a869d9baec3
Kysune <szymonlisowiec@gmail.com>
2020-07-06 18:34:13 +0200

    Changed Google Adsense client id

--- plugins/google-ads.js
+++ plugins/google-ads.js
@@ -2,7 +2,7 @@ export default () => {
   if (typeof window === 'undefined') return;
 
   const script = document.createElement('script');
-  script.dataset.adClient = 'ca-pub-7381916449670854';
+  script.dataset.adClient = 'ca-pub-6726404296831661';
   script.async = true;
   script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
   document.body.appendChild(script);
